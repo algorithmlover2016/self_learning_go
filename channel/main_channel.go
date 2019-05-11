edd4418796d38424686828c4c27c22b06808a8c6
gaoxiaoxing <gaoxiaoxing@baidu.com>
2019-05-11 23:50:15 +0800

    Print && Printlf interface{}, Printf(format, parameter)

--- channel/main_channel.go
+++ channel/main_channel.go
@@ -40,7 +40,8 @@ func main() {
         case x := <- buffer:
             fmt.Println("valid buffer:", x)
         case ch1 <- i: {
-            fmt.Println("send data %d into channel", i)
+            // fmt.Println("send data", i, "into channel")
+            fmt.Printf("send data %d into channel\n", i)
         }
         default :
             fmt.Println("do nothing")
