a04bb83151d123f9f1a4dd670c489445fc36dc44
gaoxiaoxing <gaoxiaoxing@baidu.com>
2019-05-14 10:47:58 +0800

    test nil type and function return

---
+++ func_return.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// the _ is necessary
+	if err, _ := fun1(); err == nil {
+		fmt.Println("success withour second return value")
+	}
+
+	if err, message := fun1(); err == nil {
+		fmt.Println(message)
+	}
+
+	if err, message := fun2(); err != nil {
+		fmt.Println(message)
+	}
+}
+
+func fun1() (error, string) {
+	var err error = nil
+	// err := nil // the definition and declare is not correct, because every type can be nil
+	message := fmt.Sprintf("%s", "hello world")
+	return err, message
+}
+
+func fun2() (error, string) {
+	message := fmt.Sprintf("%s", "hello world")
+	return fmt.Errorf(message), message
+}
